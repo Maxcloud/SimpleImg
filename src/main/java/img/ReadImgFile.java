51518ddd67f20c684b05b0b561ae2c1df5796d4d
Maxcloud <Maxcloud@users.noreply.github.com>
2025-05-25 10:42:40 -0400

    Updated version to 1.3

---
+++ src/main/java/img/ReadImgFile.java
@@ -0,0 +1,34 @@
+package img;
+
+import img.cache.JsonFileRepository;
+import img.io.RecyclableSeekableStream;
+import img.record.ImgCache;
+import img.snippets.ImgDataRequest;
+import img.snippets.production.SkillDataFunction;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.file.Path;
+
+@NoArgsConstructor
+@Slf4j
+public class ReadImgFile<T> {
+
+    public T fromImg(ImgDataRequest imgDataRequest, SkillDataFunction<T> bfnRequest) {
+
+        Path filePath = imgDataRequest.getFilePath();
+        String imgPath = imgDataRequest.getImgPath();
+
+        JsonFileRepository stringCache = new JsonFileRepository(filePath);
+        ImgCache imgCache = stringCache.loadFromFile();
+
+        try (RecyclableSeekableStream stream = new RecyclableSeekableStream(filePath)) {
+            WzPathNavigator root = new WzPathNavigator(imgPath, imgCache);
+            return bfnRequest.apply(stream, root);
+        } catch (Exception e) {
+            log.warn("Error reading files: {}", e.getMessage());
+            return null;
+        }
+    }
+
+}
