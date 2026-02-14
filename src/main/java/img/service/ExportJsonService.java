2ff99a9ed7452088befa0c549ad2800423909085
Maxcloud <Maxcloud@users.noreply.github.com>
2026-02-13 19:27:24 -0500

    Cleaning up ExportJsonService

--- src/main/java/img/service/ExportJsonService.java
+++ src/main/java/img/service/ExportJsonService.java
@@ -1,17 +1,13 @@
 package img.service;
 
 import img.EnvironmentConfig;
-import img.ListWzFile;
 import img.WzConfiguration;
-import img.crypto.WzStringHandler;
 import img.model.image.WzImage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.List;
 
 /**
  * SimpleImg is a utility class that traverses a directory structure,
@@ -23,53 +19,36 @@ public class ExportJsonService {
     Logger log = LoggerFactory.getLogger(ExportJsonService.class);
 
     public static void main(String[] args) {
-
         WzConfiguration configuration = new WzConfiguration();
-        EnvironmentConfig environment = configuration.getEnvironment();
-
-        Path outputDirectory = Path.of(environment.get("simple.img.output"));
-
-        int version = environment.getInt("simple.img.version");
-        byte[] secret = configuration.getSecret();
 
         System.out.println("Starting to dump strings to JSON. Please wait, this might take awhile...");
         ExportJsonService exportJsonService = new ExportJsonService();
-        exportJsonService.dumpStringsToJson(outputDirectory, version, secret);
+        exportJsonService.dumpStringsToJson(configuration);
         System.out.println("Dumping strings to JSON completed. Please double check the logs for any errors.");
     }
 
-    public void dumpStringsToJson(Path root, int version, byte[] secret) {
-        WzImage wzImage = new WzImage(version, secret);
+    public void dumpStringsToJson(WzConfiguration configuration) {
+        EnvironmentConfig environment = configuration.getEnvironment();
+        Path root = Path.of(environment.get("simple.img.output"));
+        Path list = Path.of(environment.get("simple.img.list"));
+        int version = environment.getInt("simple.img.version");
 
-        ListWzFile listWzFile = new ListWzFile(Path.of("E:\\MapleStory_83\\Maplestory\\List.wz"));
-        List<String> listWzFileNames = listWzFile.getEntries();
+        byte[] secret = configuration.getSecret();
+        WzImage image = new WzImage(secret, version, list);
 
-        WzStringHandler handle = new WzStringHandler(version, secret);
-        handle.setModernImgFiles(listWzFileNames);
-
-        try {
-            Files.walkFileTree(root, new SimpleFileVisitor<>() {
-
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-                    if (file.toString().endsWith(".json")) {
-                        return FileVisitResult.CONTINUE;
-                    }
-                    wzImage.parse(file);
-                    return FileVisitResult.CONTINUE;
-                }
-
-                @Override
-                public FileVisitResult visitFileFailed(Path file, IOException exc) {
-                    log.error("Failed to visit file: {}", file, exc);
-                    return FileVisitResult.CONTINUE;
-                }
-
-            });
-
+        try (var paths = Files.walk(root)) {
+            paths.parallel()
+                    .filter(this::fnExcluded)
+                    .filter(Files::isRegularFile)
+                    .forEach(image::parse);
         } catch (IOException e) {
             log.error("An error has occurred while walking the file tree.", e);
         }
+    }
+
+    private boolean fnExcluded(Path oPath) {
+        String sName = oPath.getFileName().toString();
+        return sName.endsWith(".json");
     }
 }
 
